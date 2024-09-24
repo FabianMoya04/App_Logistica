fbfcb8f8105621fe80ed7097e9d22c0ceaeb395f
jotik <jotikgg77@gmail.com>
2024-09-24 12:13:50 -0500

    Unir back y front

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('¡Servidor en funcionamiento!');
+});
+
+app.get('/api/users', (req, res) => {
+
+    const users = [
+        { id: 1, name: 'Juan' },
+        { id: 2, name: 'Ana' },
+        { id: 3, name: 'Pedro' }
+    ];
+    res.json(users);
 });
 
 app.listen(PORT, () => {
