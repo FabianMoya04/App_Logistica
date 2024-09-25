e9f96d9321d7a8f238066a167e8cae95709a8f06
Daikus007 <moyafabian989@gmail.com>
2024-09-24 23:45:55 -0500

    Eliminar clases con los mismos nombres, no tiene sentido por el momento

--- src/main/java/com/Logistica/AppLogistica/logica/gestionusuario/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Logistica.AppLogistica.logica.gestionusuario;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UsuarioRepository extends MongoRepository<Usuario, String> {
-    // Se puede agregar métodos personalizados aquí si es necesario
-}
