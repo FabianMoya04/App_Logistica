7bf033a2859c6d98f60f47acb86e5273c4cd9631
Daikus007 <moyafabian989@gmail.com>
2024-09-25 20:09:47 -0500

    Implementar Get y Setters en clase UsuariosORM

--- src/main/java/com/Logistica/AppLogistica/bd/orm/UsuariosORM.java
+++ src/main/java/com/Logistica/AppLogistica/bd/orm/UsuariosORM.java
@@ -18,15 +18,59 @@ public class UsuariosORM {
 
     @Id
     private ObjectId _id;
-
     private int id;
-
     private String nombre_completo;
-
     private String email;
-
     private String password;
-
     private Date fecha_registro;
+
+    public ObjectId get_id() {
+        return this._id;
+    }
+
+    public void set_id(ObjectId _id) {
+        this._id = _id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre_completo() {
+        return this.nombre_completo;
+    }
+
+    public void setNombre_completo(String nombre_completo) {
+        this.nombre_completo = nombre_completo;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getFecha_registro() {
+        return this.fecha_registro;
+    }
+
+    public void setFecha_registro(Date fecha_registro) {
+        this.fecha_registro = fecha_registro;
+    }
     
+
 }
